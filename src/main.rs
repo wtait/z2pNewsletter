566b1b4cad4361a9bb1a78d503c1e8638d799a0b
wtait <>
2022-11-21 22:31:03 -0700

    implement a greet fn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use actix_web :: {web, App, HttpRequest, HttpServer, Responder};
+use actix_web :: {HttpRequest, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
 
 fn main() {
-    println!("Hello, world!");
+
 }
